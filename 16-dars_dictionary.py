4fa419ebb5981e8287eb370a17d38618460e03be
olimovbotirbek <156398175+olimovbotirbek@users.noreply.github.com>
2024-02-21 12:28:23 +0500

    Lug'atlar

---
+++ 16-dars_dictionary.py
@@ -0,0 +1,139 @@
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+# mahsulotlar={
+#     'olma':10000,
+#     'anor':20000,
+#     'uzum':40000,
+#     'anjir':25000,
+#     'shaftoli':30000
+#     }
+
+# # print(mahsulotlar.keys())
+
+# # for mahsulot in mahsulotlar:
+# #     print(mahsulot.title())
+
+# bozorlik=['anor','uzum','non','baliq']
+
+# # for mahsulot in mahsulotlar:
+# #     if mahsulot in bozorlik:
+# #         print(f"{mahsulot.title()} - {mahsulotlar[mahsulot]} so'm")
+
+# # for mahsulot in bozorlik:
+# #     if mahsulot not in mahsulotlar:
+# #         print(f"{mahsulot}-do'konimizda mavjud emas")
+
+# print("Do'konimizda mavjud mahsulotlar:")
+# for mahsulot in sorted(mahsulotlar):
+#     print(mahsulot.title())
+    
+# lugat={'boolean':'mantiqiy qiymat',
+#        'float':"O'nlik son",
+#        'for':"biror amalni qayta-qayta bajarish tsikli",
+#        "if":"shartlarni tekshirish operatori",
+#        "integer":"butun son"}
+
+# for key,valeu in sorted(lugat.items()):
+#     print(key.capitalize(),"-",valeu.capitalize())
+    
+davlatlar={
+    "O'zbekiston":'Toshkent',
+    'aqsh':'washington d.c',
+    'rossiya':'moskva',
+    'tojikiston':'dushanbe',
+    'qirg\'iziston':'bishkek',
+    'qozog\'iston':'nursulton',
+    'malayziya':'kuala-lumpur',
+    'singapur':'sungapur',
+    'italiya':'rim'}
+
+for davlat in sorted(davlarlar):
+    print(davlat.title())
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
+    
+    
+        
+
+        
+            
+        
+    
+    
+      
+    
+
+
+
+
+    
+    
+
+    
+
+
+      
+
+    
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+      
+
+
+
+
+
+
+
+
+      
+      
